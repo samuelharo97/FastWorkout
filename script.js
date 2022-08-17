e88d2046ae937ecef5bd3edcf6ffb056b3d63403
Samuel Haro <samuel.hm@hotmail.com>
2022-08-17 14:35:51 -0300

    test version

--- script.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('.send')
-const form = document.querySelector('#form')
-
-form.addEventListener('submit', function (event) {
-  event = event || window.event
-  event.preventDefault()
-})
-
